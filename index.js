269379bf7f0560faeb11c24beae1213fce5aeb56
Reid <Reid.Finn@yahoo.com>
2021-03-05 17:29:44 -0600

    commit tree 31af14d21ada57fdc06beef47810a4810d799e09

---
+++ index.js
@@ -0,0 +1,28 @@
+//function, year,color,model
+function Scooter(year, color, model) {
+    return {
+        year,
+        color,
+        model,
+    }
+}
+
+
+
+
+
+//function Driver, name, age, experience
+function Driver(name, age, experience) {
+    return {
+        name,
+        age,
+        experience,
+    }
+}
+//function PickupLocation, address, city
+function PickupLocation(address, city) {
+    return {
+        address,
+        city,
+    }
+}
